7764a3f616f788eafaed523406e3833a63f99cdf
KevinR <K.Rohn@outlook.de>
2023-12-03 14:58:10 +0100

    added get log level function

--- src/log.c
+++ src/log.c
@@ -125,6 +125,11 @@ const char *log_level_string(log_LogLevel level)
     return level_strings[level];
 }
 
+const log_LogLevel get_log_level() {
+    log_LogLevel level = L.std_level;
+    return level;
+}
+
 void log_set_lock(log_LockFn fn, void *lock)
 {
     L.lock_fn = fn;
